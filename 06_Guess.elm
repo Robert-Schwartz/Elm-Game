f5266b6d5a579c5aa6fa6ece40d90074bd3aaa07
Robert Schwartz <bschwartz23@gmail.com>
2021-09-06 13:51:10 -0700

    set up View

--- 06_Guess.elm
+++ 06_Guess.elm
@@ -4,9 +4,13 @@ import HTML exposing (..)
 import HTML.Attributes exposing (..)
 import HTML.Eents exposing(onclick, onInput)
 
+-- ===================================================
+-- Main
 main =
 	HTML.beginnerProgram { model = model, view = view, update = update}
 
+-- ===================================================
+-- Model
 type alias Model =
 	{ word : String
 	, guess : String
@@ -19,3 +23,37 @@ model =
 
 type Msg =
 	Answer String
+
+update : Msg -> Model -> Model
+--check for a typed message
+update msg model =
+	case msg of
+		Answer txt ->
+			{ model | guess = text, isCorrect = checkIfCorrect model txt }
+
+-- define check if correct function
+-- takes 2 arguements, Type of Model and string.  returns a Boolean
+checkIfCorrect : Model -> String -> Bool
+checkIfCorrect = model txt =
+	if txt == model.word then
+	True
+	else
+		Flse
+
+-- ===================================================
+-- View
+generateResult : Model -> Html Msg
+generateResult { isCorrect} =
+	if isCorrect then
+	text "You got it!"
+	else
+		text "Nope"
+
+
+view : Model -> Html Msg
+view =
+	div []
+	[h2 [] [text ("I'm thinking of a word that starts with" ++ toString (String.slice 0 1 model.word))]
+	, input [ placeholder "Type your guess", onInput Answer]
+	, div [ generateResult model]
+	]
